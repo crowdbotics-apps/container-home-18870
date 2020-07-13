92c8d6160a4cd65b369a80e38134a2c4309ece23
Crowdbotics <team@crowdbotics.com>
2020-07-13 20:24:29 +0000

    Initial commit.

---
+++ container_home_18870/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for container_home_18870 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'container_home_18870.settings')
+
+application = get_wsgi_application()
